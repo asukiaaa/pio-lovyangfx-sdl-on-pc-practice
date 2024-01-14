82b69fb16f50749472df86d14cf3c51b222ef100
Asuki Kono <asukiaaa@gmail.com>
2024-01-14 14:40:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# pio-lovyangfx-sdl-on-pc-practice
+
+A project to handle rendering 1920x1080 for AtomDisplay and ubuntu PC.
+
+## References
+
+- [examples_for_PC/PlatformIO_SDL](https://github.com/lovyan03/LovyanGFX/tree/master/examples_for_PC/PlatformIO_SDL)
---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#ifdef USE_NATIVE
+#include <time.h>
+#define LGFX_AUTODETECT
+#include <LovyanGFX.h>
+
+#include <LGFX_AUTODETECT.hpp>
+#else
+#include <M5AtomDisplay.h>
+#endif
+
+#include <Arduino.h>
+#include <stdlib.h>
+#include <string_asukiaaa.h>
+#include <unistd.h>
+
+#include <iostream>
+
+#if defined(SDL_h_)
+// static LGFX lcd ( 320, 240, 2 );
+static LGFX lcd(1080, 1920, 2);
+// static LGFX lcd(1920, 1080, 2);
+#else
+M5AtomDisplay lcd(1920, 1080);
+#endif
+
+void setup() {
+  lcd.init();
+#ifndef USE_NATIVE
+  lcd.setRotation(0);
+#endif
+  lcd.setTextSize(8);
+  lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+}
+
+void loop() {
+#ifdef USE_NATIVE
+  auto seconds = time(NULL);
+#else
+  auto seconds = millis() / 1000;
+#endif
+  // print(string_asukiaaa::padStart("hi", 4, ' ').c_str());
+  // print("\n");
+  // // printf("hello\n");
+  // fflush(stdout);
+  // std::cout << "hello" << std::endl;
+  lcd.setCursor(0, 0);
+  lcd.println("top left");
+  lcd.print("string test ");
+  lcd.println(string_asukiaaa::padStart("abcd", 10, '-').c_str());
+  lcd.println(("at " + String(seconds)).c_str());
+  lcd.drawRightString("bottom right", lcd.width(),
+                      lcd.height() - lcd.fontHeight());
+#ifdef USE_NATIVE
+  sleep(1);
+#else
+  delay(1000);
+#endif
+}
+
+#if defined(SDL_h_)
+int user_func(bool* running) {
+  setup();
+  do {
+    loop();
+  } while (*running);
+  return 0;
+}
+
+int main(int, char**) { return lgfx::Panel_sdl::main(user_func); }
+#endif
